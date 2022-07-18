1e9bfa1c83c2b089173893107d91a65b9d4bdbb9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-18 23:09:43 +0100

    Updating the quotes

--- calculate.js
+++ calculate.js
@@ -1,44 +1,44 @@
-let display_section = document.getElementById("display");
-let buttons = Array.from(document.getElementsByClassName("button"));
+let display_section = document.getElementById('display')
+let buttons = Array.from(document.getElementsByClassName('button'))
 
 buttons.map((button) => {
-	button.addEventListener("click", (el) => {
-		doCalculate(el);
-	});
-});
+	button.addEventListener('click', (el) => {
+		doCalculate(el)
+	})
+})
 
 function doCalculate(e) {
-	if (display_section.innerText == "0") {
-		display_section.innerText = "";
+	if (display_section.innerText == '0') {
+		display_section.innerText = ''
 	}
 
 	switch (e.target.innerText) {
-		case "AC":
-			display_section.innerText = "0";
-			break;
+		case 'AC':
+			display_section.innerText = '0'
+			break
 
-		case "DEL":
-			if (display_section.innerText != "Error") {
-				display_section.innerText = display_section.innerText.slice(0, -1);
+		case 'DEL':
+			if (display_section.innerText != 'Error') {
+				display_section.innerText = display_section.innerText.slice(0, -1)
 			}
-			if (display_section.innerText == "") {
-				display_section.innerText = "0";
+			if (display_section.innerText == '') {
+				display_section.innerText = '0'
 			}
 
-			break;
+			break
 
-		case "=":
+		case '=':
 			try {
-				display_section.innerText = eval(display_section.innerText);
+				display_section.innerText = eval(display_section.innerText)
 			} catch {
-				display_section.innerText = "Error";
+				display_section.innerText = 'Error'
 			}
-			break;
+			break
 
 		default:
-			if (display_section.innerText != "Error") {
-				display_section.innerHTML += e.target.innerText;
+			if (display_section.innerText != 'Error') {
+				display_section.innerHTML += e.target.innerText
 			}
-			break;
+			break
 	}
 }
